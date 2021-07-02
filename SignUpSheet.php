4d95c21817551e0b38bd8a744f2d70d691c54814
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-07-02 16:21:05 -0400

    Update SignUpSheet.php

--- SignUpSheet.php
+++ SignUpSheet.php
@@ -1,3 +1,66 @@
+<?php
+  $title = $_POST['title'] ?? null;
+  $description = $_POST['description'] ?? null;
+  $location = $_POST['location'] ?? null;
+  $privacy = $_POST['status'] ?? null;
+  $numSlots = $_POST['numSlots'] ?? null;
+  $notes = $_POST['notes'] ?? null;
+  $errors = array();
+  session_start();
+  if(!isset($_SESSION['username'])){
+    header("Location:login.php");
+    exit();
+  }
+  $creator = $_SESSION['username'];
+
+  if (isset($_POST['submit'])){
+
+    //sanitize all the textbox inputs
+    $description = filter_var($description, FILTER_SANITIZE_STRING);
+    $notes = filter_var($notes, FILTER_SANITIZE_STRING);
+    //validate user has entered a title
+    if (!isset($title) || strlen($title) === 0) {
+      $errors['title'] = true;
+    }
+
+    //validate user has entered a title
+    if (!isset($creator) || strlen($creator) === 0) {
+      $errors['creator'] = true;
+    }
+
+    //validate user has entered a title
+    if (!isset($description)) {
+      $errors['description'] = true;
+    }
+
+    //validate user has entered a title
+    if (!isset($location) || strlen($location) === 0) {
+      $errors['location'] = true;
+    }
+
+    //make sure the chose a character
+    if (empty($privacy)) {
+      $errors['privacy'] = true;
+    }
+
+    //make sure they agreed to the terms
+    if ($numSlots == "") {
+      $errors['numSlots'] = true;
+    }
+
+      //only do this if there weren't any errors
+      if (count($errors) === 0) {
+        $_SESSION['title'] = $title;
+        $_SESSION['description'] = $description;
+        $_SESSION['location'] = $location;
+        $_SESSION['privacy'] = $privacy;
+        $_SESSION['notes'] = $notes;
+        $_SESSION['numSlots'] = $numSlots;
+        header("Location:generateSlots.php");
+        exit();
+      }
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -21,46 +84,50 @@
         </nav>
         <section>
           <h2>Sign-Up Sheet Details</h2>
-          <form id="requestform" action="SignUpSheet.php" method="post">
+          <form action="<?=htmlentities($_SERVER['PHP_SELF'])?>" method="POST" autocomplete="off">
             <div>
-              <label for="title">Sign-Up Sheet Title</label>
-              <input id="title" name="title" type="text" placeholder="Project Check-In #1" />
+              <label for="title">Title</label>
+              <input id="title" name="title" type="text" placeholder="Project Check-In #1"value="<?=$title?>"/>
+              <span class="error <?=!isset($errors['title']) ? 'hidden' : "";?>">Please enter a title</span>
             </div>
             <div>
-              <label for="description">Sign-Up Sheet Description</label>
-              <textarea name="description" id="description" cols="30" rows="10"></textarea>
+              <label for="creator">Creator</label>
+              <input id="creator" name="creator" type="text" value="<?=$creator?>"/>
+              <span class="error <?=!isset($errors['creator']) ? 'hidden' : "";?>">Please enter your username</span>
             </div>
             <div>
-              <label for="slots">Number of slots</label>
-              <select name="primary" id="primary">
-                <option value="">Choose One</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="o">Other</option>
-              </select>
+              <label for="description">Description</label>
+              <textarea name="description" id="description" cols="30" rows="10"><?=$description?></textarea>
+              <span class="error <?=!isset($errors['description']) ? 'hidden' : "";?>">Please enter a description</span>
+            </div>
+            <div>
+              <label for="location">Loaction</label>
+              <input id="location" name="location" type="text" placeholder="Remote via Zoom" value="<?=$location?>"/>
+              <span class="error <?=!isset($errors['location']) ? 'hidden' : "";?>">Please enter a location</span>
             </div>
             <fieldset>
               <legend>Privacy</legend>
               <div>
-                <input id="public" name="status" type="radio" value="O" />
+                <input id="public" name="status" type="radio" value="public" <?=$privacy == "public" ? 'checked' : ''?>/>
                 <label for="public">Public</label>
               </div>
-
               <div>
-                <input id="private" name="status" type="radio" value="C" />
+                <input id="private" name="status" type="radio" value="private" <?=$privacy == "private" ? 'checked' : ''?>/>
                 <label for="private">Private</label>
               </div>
+              <span class="error <?=!isset($errors['privacy']) ? 'hidden' : "";?>">Please select a privacy setting</span>
             </fieldset>
             <div>
-              <button id="submit"><a href="SheetThanks.php">Create Sheet</a></button>
+              <label for="notes">Notes</label>
+              <textarea name="notes" id="notes" cols="30" rows="10"><?=$notes?></textarea>
+            </div>
+              <div>
+                <label for="numSlots">Number of Time Slots</label>
+                <input id="numSlots" name="numSlots" type="number" value="<?=$numSlots?>"/>
+                <span class="error <?=!isset($errors['numSlots']) ? 'hidden' : "";?>">Please enter the number of time slots in this sheet</span>
+              </div>
+            <div>
+              <button type="submit" name="submit">Generate Slots</button>
             </div>
           </form>
         </section>

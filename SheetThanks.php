50e251c368fe939c8ad1d2b7f3734357ad412553
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-07-02 16:30:06 -0400

    Update SheetThanks.php

--- SheetThanks.php
+++ SheetThanks.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Done!</title>
     <link rel="stylesheet" href="styles/master.css" />
-</head>
-<body>
-  <?php include 'includes/navbar.php';?>
+  </head>
+  <body>
+    <section class = "mySignUps">
+      <?php include 'includes/navbar.php';?>
       <header>
         <h1><i class="fas fa-clipboard-check"></i> All Done!</h1>
-        </header>
-        <main>
-            <nav>
-              <ul>
-                <li><a href="mySignups.php">Home</a></li>
-              </ul>
-            </nav>
-  <section>
-    <h3>Your Sign-Up Sheet has been Created!</h3>
-  </section>
-  </main>
-</body>
+      </header>
+      <main>
+        <nav>
+          <ul>
+            <li><a href="mySignups.php">Home</a></li>
+          </ul>
+        </nav>
+        <section>
+          <h3>Your Sign-Up Sheet has been Created!</h3>
+        </section>
+      </main>
+    </section>
+  </body>
 </html>
+

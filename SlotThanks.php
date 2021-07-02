f49480066649125932d625ca8cd56069f377936f
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-07-02 16:29:41 -0400

    Update SlotThanks.php

--- SlotThanks.php
+++ SlotThanks.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Done!</title>
-    <link rel="stylesheet" href="styles/master.css" />
-</head>
-<body>
-  <?php include 'includes/navbar.php';?>
-      <header>
-        <h1><i class="fas fa-clipboard-check"></i> Time Slot Booked!</h1>
+  <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Done!</title>
+      <link rel="stylesheet" href="styles/master.css" />
+  </head>
+  <body>
+    <section class = "mySignUps">
+      <?php include 'includes/navbar.php';?>
+        <header>
+          <h1><i class="fas fa-clipboard-check"></i> Time Slot Booked!</h1>
         </header>
         <main>
-            <nav>
-              <ul>
-                <li><a href="mySignups.php">Home</a></li>
-              </ul>
-            </nav>
-      <section>
-        <h3>You are now registered for this time slot!</h3>
-      </section>
-  </main>
-</body>
+          <nav>
+            <ul>
+              <li><a href="mySignups.php">Home</a></li>
+            </ul>
+          </nav>
+          <section>
+            <h3>You are now registered for this time slot!</h3>
+          </section>
+      </main>
+    </section>
+  </body>
 </html>

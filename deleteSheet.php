d6ca5c351c70fe05eaab3ea33786c75092700e51
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-07-03 17:18:29 -0400

    Create deleteSheet.php

---
+++ deleteSheet.php
@@ -0,0 +1,39 @@
+<?php 
+  include 'includes/library.php';
+  $pdo = connectDB();
+
+  $query = "delete from timeslot_slots where sheetID = ?";
+  $stmt = $pdo->prepare($query);
+  $stmt->execute([$_GET["id"]]);
+
+  $query = "delete from timeslot_sheets where ID = ?";
+  $stmt = $pdo->prepare($query);
+  $stmt->execute([$_GET["id"]]);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Done!</title>
+      <link rel="stylesheet" href="styles/master.css" />
+  </head>
+  <body>
+    <section class = "mySignUps">
+      <?php include 'includes/navbar.php';?>
+        <header>
+          <h1><i class="fas fa-clipboard-check"></i> Sheet Deleted!</h1>
+        </header>
+        <main>
+          <nav>
+            <ul>
+              <li><a href="mySignups.php">Home</a></li>
+            </ul>
+          </nav>
+          <section>
+            <h3>This sheet has now been deleted!</h3>
+          </section>
+      </main>
+    </section>
+  </body>
+</html>

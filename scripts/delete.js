6c1aefcb20350844ff6ba812e092289916faca77
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-08-02 17:49:14 -0400

    Delete delete.js

--- scripts/delete.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//confirmation dialog on delete
-const deleteButton = document.querySelector("div ul li");
-deleteButton.addEventListener("click", (ev) =>{
-    let confirmDelete = confirm ("Are you sure you want to delete this sheet?");
-    if (confirmDelete == false){
-        ev.preventDefault();
-    }
-});

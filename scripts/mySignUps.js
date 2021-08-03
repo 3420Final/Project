d065b0b210b6fa20d89851ed53da63809c67ef3a
jjleneve <84870606+jjleneve@users.noreply.github.com>
2021-08-03 15:01:04 -0400

    Update mySignUps.js

--- scripts/mySignUps.js
+++ scripts/mySignUps.js
@@ -1,23 +1,55 @@
 "use strict";
 //confirmation dialog on delete
-const sheetListItems = document.querySelectorAll(".Sign-upSheets div ul li");
-if (sheetListItems[3] != undefined){
-    sheetListItems[3].addEventListener("click", (ev) =>{
-        let confirmDelete = confirm ("Are you sure you want to delete this sheet?");
-        if (confirmDelete == false){
-            ev.preventDefault();
-        }
-    });
+//Mouse delete sheet
+const sheetListItems = document.querySelectorAll(".Sign-upSheets div ul");
+for(let i = 0; i < sheetListItems.length; i ++){
+    if (sheetListItems[i].lastElementChild != undefined){
+        sheetListItems[i].lastElementChild.addEventListener("click", (ev) =>{
+            let confirmSheetDelete = confirm ("Are you sure you want to delete this sheet?");
+            if (confirmSheetDelete == false){
+                ev.preventDefault();
+            }
+        });
+    }
 }
-const slotListItems = document.querySelectorAll(".Slots div ul li");
-if (slotListItems[1] != undefined){
-    slotListItems[1].addEventListener("click", (ev) =>{
-        let confirmDelete = confirm ("Are you sure you want to delete this time slot?");
-        if (confirmDelete == false){
-            ev.preventDefault();
-        }
-    });
+
+//Mouse delete time slot
+const slotListItems = document.querySelectorAll(".Slots div ul");
+for(let i = 0; i < slotListItems.length; i ++){
+    if (slotListItems[i].lastElementChild != undefined){
+        slotListItems[i].lastElementChild.addEventListener("click", (ev) =>{
+            let confirmSheetDelete = confirm ("Are you sure you want to delete this sheet?");
+            if (confirmSheetDelete == false){
+                ev.preventDefault();
+            }
+        });
+    }
 }
-//at least 2 plug-ins ( One could be password strength on create account)
-//we could use flatpickr which is some kind of calender plug-in
-//we can think about what plug-ins to use
+
+//Keyboard delete sheet
+for(let i = 0; i < sheetListItems.length; i ++){
+    if (sheetListItems[i].lastElementChild != undefined){
+        sheetListItems[i].lastElementChild.addEventListener("click", (ev) =>{
+            if (ev.keyCode === 13){
+                let confirmSheetDeleteKey = confirm ("Are you sure you want to delete this sheet?");
+                if (confirmSheetDeleteKey == false){
+                    ev.preventDefault();
+                }
+            }
+        });
+    }
+}
+
+//Keyboard delete time slot
+for(let i = 0; i < slotListItems.length; i ++){
+    if (slotListItems[i].lastElementChild != undefined){
+        slotListItems[i].lastElementChild.addEventListener("click", (ev) =>{
+            if (ev.keyCode === 13){
+                let confirmSlotDeleteKey = confirm ("Are you sure you want to delete this time slot?");
+                if (confirmSlotDeleteKey == false){
+                    ev.preventDefault();
+                }
+            }
+        });
+    }
+}
